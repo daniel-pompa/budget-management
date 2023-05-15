9856c2baebc555800ed8f09349bf3ba772707dcd
Daniel <daniel.40.pompa@gmail.com>
2023-05-16 01:00:38 +0200

    Display icon to create a new expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import NewExpenseIcon from './assets/images/new-expense-icon.svg';
 
 function App() {
   // Create budget state
@@ -14,6 +15,16 @@ function App() {
         bugetIsValid={bugetIsValid}
         setBugetIsValid={setBugetIsValid}
       />
+
+      {/* When the budget is valid, the icon to create a new expense is displayed */}
+      {bugetIsValid && (
+        <>
+          <main>{/* TODO Display ExpenseList component */}</main>
+          <div className='new-expense'>
+            <img src={NewExpenseIcon} alt='new expense icon' />
+          </div>
+        </>
+      )}
     </div>
   );
 }
