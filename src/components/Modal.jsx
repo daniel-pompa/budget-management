b4812a5e65dfac591c748f0c934f0e540b1028da
Daniel <daniel.40.pompa@gmail.com>
2023-05-16 22:35:14 +0200

    Create state of expenses and validate form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import Alert from './Alert';
 import CloseButton from '../assets/images/close-icon.svg';
 
 const Modal = ({ setModal, animateModal, setAnimateModal }) => {
+  // Create state of expenses
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('');
+
+  // Create alert state to validate the form
+  const [alert, setAlert] = useState('');
+
+  // Function to close modal window
   const hideModal = () => {
     setAnimateModal(false);
 
     setTimeout(() => {
       setModal(false);
     }, 300);
+  };
+
+  // Function that is executed when the user submits the form.
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Validate form
+    if ([title, amount, category].includes('')) {
+      setAlert('Todos los campos son obligatorios');
+
+      setTimeout(() => {
+        setAlert('');
+      }, 3000);
+
+      return;
+    }
+
+    // TODO Save expense
   };
 
   return (
@@ -17,21 +46,40 @@ const Modal = ({ setModal, animateModal, setAnimateModal }) => {
       </div>
 
       {/* If the state of animateModal is true add the animate class */}
-      <form className={`form ${animateModal ? 'animate' : 'close'}`}>
+      <form
+        onSubmit={handleSubmit}
+        className={`form ${animateModal ? 'animate' : 'close'}`}
+      >
         <legend>Nuevo Gasto</legend>
+        {alert && <Alert type='error'>{alert}</Alert>}
         <div className='field'>
           <label htmlFor='title'>Título</label>
-          <input id='title' type='text' placeholder='Título' />
+          <input
+            id='title'
+            type='text'
+            placeholder='Título'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
         </div>
 
         <div className='field'>
           <label htmlFor='amount'>Cantidad</label>
-          <input id='amount' type='number' min={0} />
+          <input
+            id='amount'
+            type='number'
+            value={amount}
+            onChange={e => setAmount(Number(e.target.value))}
+          />
         </div>
 
         <div className='field'>
           <label htmlFor='category'>Categoría</label>
-          <select id='category'>
+          <select
+            id='category'
+            value={category}
+            onChange={e => setCategory(e.target.value)}
+          >
             <option value=''>Seleccionar</option>
             <option value='ahorro'>Ahorro</option>
             <option value='hogar'>Hogar</option>
