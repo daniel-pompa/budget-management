339fb34b51c83c39375cddb473ffa91ddde515bb
Daniel <daniel.40.pompa@gmail.com>
2023-05-19 00:11:03 +0200

    Customise chart

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { formatAmount } from '../helpers';
 
 const Budget = ({ expenses, budget }) => {
   // State for available and spent balance
+  const [percentage, setPercentage] = useState(0);
   const [available, setAvailable] = useState(0);
   const [spent, setSpent] = useState(0);
 
@@ -21,14 +22,28 @@ const Budget = ({ expenses, budget }) => {
     // Total balance available in budget
     const totalAvailable = budget - totalSpent;
 
+    // Calculate the percentage spent of the budget
+    const newPercentage = (((budget - totalAvailable) / budget) * 100).toFixed(2);
+
     setSpent(totalSpent);
     setAvailable(totalAvailable);
+    setTimeout(() => {
+      setPercentage(newPercentage);
+    }, 1000);
   }, [expenses]);
 
   return (
     <div className='budget-container container shadow two-columns'>
       <div>
-        <CircularProgressbar value={0} />
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#3b82f6',
+            trailColor: '#f5f5f5',
+            textColor: '#3b82f6',
+          })}
+          value={percentage}
+          text={`${percentage}% Gastado`}
+        />
       </div>
 
       <div className='budget-content'>
